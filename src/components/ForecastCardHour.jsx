decce9d2b935527d6e44aa37c037dd9ddff97f6b
diclementef92 <diclementef92@gmail.com>
2023-02-18 19:04:27 +0100

    stile temperatura, icone, date

--- src/components/ForecastCardHour.jsx
+++ src/components/ForecastCardHour.jsx
@@ -1,20 +1,41 @@
 import { Card, Col } from "react-bootstrap";
+import { BsFillCaretUpFill } from "react-icons/bs";
+import { BsFillCaretDownFill } from "react-icons/bs";
 
 const ForecastCardHour = (props) => {
-  let hour = props.forecast.dt_txt.split(" ")[1];
+  // let hour = props.forecast.dt_txt.split(" ")[1];
+  let time = new Date(props.forecast.dt_txt);
 
   return (
-    <Col className="mt-4" xs={4}>
-      <Card className="shadow-lg">
+    <Col className="h-100" xs={4}>
+      <Card className="mt-4 shadow-lg text-center">
         <Card.Img
+          className=""
           variant="top"
           src={`https://openweathermap.org/img/wn/${props.forecast.weather[0].icon}@4x.png`}
         />
         <Card.Body>
-          <Card.Title>Ore: {hour}</Card.Title>
-          <Card.Title>{props.forecast.main.temp}°C</Card.Title>
-          {/* <Card.Text>{props.forecast.weather[0].description}</Card.Text> */}
-          <Card.Text>Umidità: {props.forecast.main.humidity}%</Card.Text>
+          <Card.Text className="temp">
+            <Card.Text>{props.forecast.main.temp}°C</Card.Text>
+            <span className="max">
+              <BsFillCaretUpFill />
+              {props.forecast.main.temp_max}
+            </span>
+
+            <span className="min">
+              <BsFillCaretDownFill />
+              {props.forecast.main.temp_min}
+            </span>
+          </Card.Text>
+          <Card.Text className="descr">
+            {props.forecast.weather[0].description}
+          </Card.Text>
+          <Card.Text className="time ">
+            {time.getHours() < 10 ? "0" + time.getHours() : time.getHours()}:
+            {time.getMinutes() < 10
+              ? "0" + time.getMinutes()
+              : time.getMinutes()}
+          </Card.Text>
         </Card.Body>
       </Card>
     </Col>
--- src/components/WeatherCardNow.jsx
+++ src/components/WeatherCardNow.jsx
@@ -1,18 +1,40 @@
 import { Card, Col } from "react-bootstrap";
+import { BsFillCaretUpFill } from "react-icons/bs";
+import { BsFillCaretDownFill } from "react-icons/bs";
 
 const WeatherCardNow = (props) => {
   return (
-    <Col className="mt-4">
-      <Card className="shadow-lg">
+    <Col className="">
+      <Card className=" mt-4 shadow-lg">
         <Card.Img
           variant="top"
           src={`https://openweathermap.org/img/wn/${props.singleDay.weather[0].icon}@4x.png`}
         />
         <Card.Body>
-          <Card.Title>{props.singleDay.name} - Adesso</Card.Title>
-          <Card.Title>{props.singleDay.main.temp}°C</Card.Title>
-          <Card.Text>{props.singleDay.weather[0].description}</Card.Text>
-          <Card.Text>Umidità: {props.singleDay.main.humidity}%</Card.Text>
+          <Card.Text className="city">
+            {props.singleDay.name} - Adesso
+          </Card.Text>
+          <Card.Text className="temp">
+            <Card.Text>
+              {props.singleDay.main.temp}
+              °C
+            </Card.Text>
+            <span className="max">
+              <BsFillCaretUpFill />
+              {props.singleDay.main.temp_max}
+            </span>
+
+            <span className="min">
+              <BsFillCaretDownFill />
+              {props.singleDay.main.temp_min}
+            </span>
+          </Card.Text>
+          <Card.Text className="descr">
+            {props.singleDay.weather[0].description}
+          </Card.Text>
+          <Card.Text className="humidity">
+            Umidità: {props.singleDay.main.humidity}%
+          </Card.Text>
         </Card.Body>
       </Card>
     </Col>
